1a46dfe67fcee9d0df227f9b87fe8b058825c184
Jasio <mcyanek@gmail.com>
2020-10-08 20:22:21 +0200

    implements naive wannabe C++ HashTable, unfortunatelly it is work on legacy code, so it doesn't work well with maven, it could be compiled by javac, and then works properly with code provided in zip file

--- src/main/java/com/codecool/HashTable.java
+++ src/main/java/com/codecool/HashTable.java
@@ -1,4 +1,6 @@
-package com.codecool;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -13,52 +15,67 @@ package com.codecool;
 
 public class HashTable
 {
-	/**
-   * The constructor is given a table size (i.e. how big to make the array)
-   * and a StringHasher, which is used to hash the strings.
-   *
-   * @param tableSize number of elements in the hash array
-   *        hasher    Object that creates the hash code for a string
-   * @see StringHasher
-   */
-	public HashTable(int tableSize, StringHasher hasher)
-	{
 
+	/**
+	 * The constructor is given a table size (i.e. how big to make the array)
+	 * and a StringHasher, which is used to hash the strings.
+	 *
+	 * @param tableSize number of elements in the hash array
+	 *        hasher    Object that creates the hash code for a string
+	 * @see StringHasher
+	 */
+
+	StringHasher stringHasher;
+	int tableSize;
+	List<LinkedList<String>> hashTable;
+
+	public HashTable(int tableSize, StringHasher hasher)
+
+	{
+		this.stringHasher = hasher;
+		this.tableSize = tableSize;
+		this.hashTable = new ArrayList<>(tableSize);
+		for (int i = 0; i<tableSize; i++){
+			hashTable.add(new LinkedList<String>());
+		}
 	}
 
 
 	/**
-   * Takes a string and adds it to the hash table, if it's not already
-   * in the hash table.  If it is, this method has no effect.
-   *
-   * @param s String to add
-   */
+	 * Takes a string and adds it to the hash table, if it's not already
+	 * in the hash table.  If it is, this method has no effect.
+	 *
+	 * @param s String to add
+	 */
 	public void add(String s)
 	{
-
+		int hash = stringHasher.hash(s)%tableSize;
+		if (hashTable.get(hash).isEmpty() || !lookup(s)) hashTable.get(hash).add(s);
 	}
-	
+
 
 	/**
-  * Takes a string and returns true if that string appears in the
-	* hash table, false otherwise.
-  *
-  * @param s String to look up
-  */
+	 * Takes a string and returns true if that string appears in the
+	 * hash table, false otherwise.
+	 *
+	 * @param s String to look up
+	 */
 	public boolean lookup(String s)
 	{
-		return false;
+		int hash = stringHasher.hash(s)%tableSize;
+		return hashTable.get(hash).contains(s);
 	}
-	
+
 
 	/**
-   * Takes a string and removes it from the hash table, if it
-   * appears in the hash table.  If it doesn't, this method has no effect.
-   *
-   * @param s String to remove
-  */
+	 * Takes a string and removes it from the hash table, if it
+	 * appears in the hash table.  If it doesn't, this method has no effect.
+	 *
+	 * @param s String to remove
+	 */
 	public void remove(String s)
 	{
-
+		int hash = stringHasher.hash(s)%tableSize;
+		if (lookup(s)) hashTable.get(hash).remove(s);
 	}
 }
